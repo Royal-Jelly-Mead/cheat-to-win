4aeab8d8198f7df6689813f222fafb7fd87e62cf
Joshua Bracks <joshabracks@gmail.com>
2023-02-07 18:11:45 -0800

    cleanup

--- src/client/gameplay/index.js
+++ src/client/gameplay/index.js
@@ -1,7 +1,5 @@
-
 import Phaser from "phaser"
 import config from "./util/config.js"
-
 
 config.scene.preload = () =>
 {
@@ -16,32 +14,3 @@ config.scene.update = () =>
 }
 
 const game = new Phaser.Game(config)
-// const body = document.querySelector('body')
-
-// const LOAD_ORDER = [
-//     "./gameplay/util/phaser.js",
-//     "./gameplay/util/config.js",
-//     "./gameplay/renderTest.js",
-// ]
-
-// let game;
-
-// async function loadElements() {
-//     const script = LOAD_ORDER.shift()
-//     if (script === undefined) {
-//         game = new Phaser.Game(config)
-//         body.removeChild(document.querySelector('#index'))
-//         return;
-//     }
-//     const element = document.createElement('script')
-//     element.src = script;
-//     body.appendChild(element);
-//     return new Promise(async (resolve) => {
-//         element.onload = () => {
-//             body.removeChild(element)
-//             resolve(loadElements())
-//         }
-//     })
-// }
-
-// loadElements()
