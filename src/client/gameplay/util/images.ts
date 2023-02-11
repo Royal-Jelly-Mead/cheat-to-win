4c9dd87a2d63e73b080b6fe3acca24becffb1232
Joshua Bracks <joshabracks@gmail.com>
2023-02-10 22:28:27 -0800

    post typescript migration cleanup

--- src/client/gameplay/util/images.ts
+++
@@ -1,9 +0,0 @@
-// export function loadImagesToScene(images, scene) {
-//     Object.keys(images).forEach(name => {
-//         scene.load.image(name, images[name]);
-//     })
-// }
-
-// export default {
-//     loadImagesToScene
-// }
