a4781b1b796126720782ec7383386cfc63fc65d9
Joshua Bracks <joshabracks@gmail.com>
2023-02-16 22:24:26 -0800

    lots of TODO

--- game-state/gameStateController.ts
+++ game-state/gameStateController.ts
@@ -4,11 +4,21 @@ import fetch from 'node-fetch'
 import { Player } from '../game-client/gameplay/components/player'
 import { Item, EquipmentSlot, Weapon, Bodygear, Handgear, Footwear, Consumable } from '../game-client/gameplay/components/item'
 
+const MAP_SIZE = MAP_CONFIG.defaultMapSize
+const API = 'https://localhost:3000'
+
+// type Command = {}
+
+type PlayerInput = {
+  matchID: number
+  playerID: number
+  // command: Command
+}
+
 const MATCHES: GameStateValues[] = []
 const PLAYER_GROUPS: Player[][] = []
 const ACTIVE_STATUS: boolean[] = []
-const MAP_SIZE = MAP_CONFIG.defaultMapSize
-const API = 'https://localhost:3000'
+const INPUT_QUEUES: PlayerInput[][]
 
 const STANDARD_ITEM_SET: Item[] = (() => {
   const result: Item[] = []
@@ -47,6 +57,7 @@ async function initMatch(players: Player[]) {
         MATCHES[i] = gameStateValues
         PLAYER_GROUPS[i] = players
         ACTIVE_STATUS[i] = true
+        INPUT_QUEUES[i] = []
         gameID = i
         break
       }
@@ -55,9 +66,16 @@ async function initMatch(players: Player[]) {
       gameID = MATCHES.length
       MATCHES.push(gameStateValues)
       PLAYER_GROUPS.push(players) // probably useful to re-connect players to dropped game
+      ACTIVE_STATUS.push(true)
+      INPUT_QUEUES.push([])
     }
-    // TODO: broadcast gamestart to players
+    
+    // TODO: broadcast gamestart to all players in match and establish ws room for the player group (if not already handled)
   } catch (e) {
     // TODO: broadcast error to players
   }
+}
+
+function update(gameIndex: number) {
+  // TODO Process Player Commands and update world state
 }
